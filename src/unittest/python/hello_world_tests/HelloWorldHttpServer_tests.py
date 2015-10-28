8ffff41188f5e55557e37e278179d5a6892113df
Konrad Hosemann <konrad@hosemann.name>
2015-10-28 15:57:06 +0100

    fix test

--- src/unittest/python/hello_world_tests/HelloWorldHttpServer_tests.py
+++ src/unittest/python/hello_world_tests/HelloWorldHttpServer_tests.py
@@ -1,6 +1,6 @@
 import unittest
+
 import webtest
-from hello_world import HelloWorldHttpServer
 from bottle import default_app
 
 app = default_app()
@@ -12,9 +12,6 @@ class HelloWorldTests(unittest.TestCase):
 
     def test_hello_world(self):
         resp = test_app.get("/")
-        self.assertEqual(resp.text, "<h2>Hello World!</h2><br/><b>Build Version:</b> <i>${build_version}</i>")
+        self.assertEqual(resp.text, "<html><body><h2>Hello World!</h2><br/><b>Build Version:</b> <i>${build_version}</i></body></html>")
         self.assertEqual(resp.status, '200 OK')
 
-    def test_hello_world_not_equals(self):
-        resp = test_app.get("/")
-        self.assertNotEqual(resp.text, "<h2>Hello World!</h2><br/><b>Build Version:</b> <i></i>")
