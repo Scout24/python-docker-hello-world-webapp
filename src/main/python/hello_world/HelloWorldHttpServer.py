54e7c5487fc7393a5e471eec53e3ea99b903750d
Konrad Hosemann <konrad@hosemann.name>
2015-12-02 09:21:59 +0100

    say hello with name

--- src/main/python/hello_world/HelloWorldHttpServer.py
+++ src/main/python/hello_world/HelloWorldHttpServer.py
@@ -5,11 +5,12 @@ __version__ = '${build_version}'
 app = default_app()
 
 
-@app.route("/")
-def hello_world():
-    return ('<html><body><h2>Hello World!</h2>'
+@app.route('/')
+@app.route('/<name>')
+def hello_world(name='World'):
+    return ('<html><body><h2>Hello {0}!</h2>'
             '<br/><b>Build Version:</b>'
-            ' <i>{0}</i></body></html>').format(__version__)
+            ' <i>{1}</i></body></html>').format(name, __version__)
 
 
 def run_server():  # pragma: no cover
